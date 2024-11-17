21c41cf9c16c66fdf67c206ad1dffccb63653871
Sasiru Ravihansa <sasiruravihansa@gmail.com>
2024-11-17 14:54:28 +0530

    added session to the backend and alert messages

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,3 +1,4 @@
+// package main
 package main
 
 import (
@@ -99,5 +100,8 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		app.serverError(w, err)
 		return
 	}
+
+	app.sessionManager.Put(r.Context(), "flash", "snippet successfully created!")
+
 	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
