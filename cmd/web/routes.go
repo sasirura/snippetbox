52eacf73c1bac68890773b6bfb33839b17385d0d
Sasiru Ravihansa <sasiruravihansa@gmail.com>
2024-11-22 22:44:45 +0530

    added request context

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -18,7 +18,7 @@ func (app *application) routes() http.Handler {
 	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
 	// unprotected
-	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf)
+	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 
 	// snippets
 	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
